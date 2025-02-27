8124c51350fe88dea3d8bdb4c0c0e5ee99b7c373
angela <angelamlorenzo@gmail.com>
2025-02-27 15:38:15 +0100

    add basic sections

--- src/app/portfolio-module/portfolio.module.ts
+++ src/app/portfolio-module/portfolio.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GalleryComponent } from './components/gallery/gallery.component';
-import { MainPageComponent } from './pages/main-page/main-page.component';
-import { TabsGalleryComponent } from './components/tabs-gallery/tabs-gallery.component';
-import { filterGalleryPipe } from './utils/filterGallery.pipe';
-import { SlideComponent } from './components/slide/slide.component';
-import { ModalDetailComponent } from './components/modal-detail/modal-detail.component';
-import { SpinnerComponent } from './components/spinner/spinner.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { GalleryComponent } from "./components/gallery/gallery.component";
+import { MainPageComponent } from "./pages/main-page/main-page.component";
+import { TabsGalleryComponent } from "./components/tabs-gallery/tabs-gallery.component";
+import { filterGalleryPipe } from "./utils/filterGallery.pipe";
+import { SlideComponent } from "./components/slide/slide.component";
+import { ModalDetailComponent } from "./components/modal-detail/modal-detail.component";
+import { SpinnerComponent } from "./components/spinner/spinner.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { CoverComponent } from "./components/cover/cover.component";
+import { ProgressBarsComponent } from "./components/progress-bars/progress-bars.component";
+import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
 
 @NgModule({
-  declarations: [GalleryComponent, MainPageComponent, TabsGalleryComponent, filterGalleryPipe, SlideComponent, ModalDetailComponent, SpinnerComponent, NavbarComponent],
+  declarations: [
+    GalleryComponent,
+    MainPageComponent,
+    TabsGalleryComponent,
+    filterGalleryPipe,
+    SlideComponent,
+    ModalDetailComponent,
+    SpinnerComponent,
+    NavbarComponent,
+    CoverComponent,
+    ProgressBarsComponent,
+    WorkExperienceComponent,
+  ],
   imports: [CommonModule],
   exports: [GalleryComponent, MainPageComponent],
 })
